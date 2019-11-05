bb68c6c1b61688e3646a0956299ee39ef1ab42e9
OminousFlake <rus48goldfinch@gmail.com>
2019-11-06 00:06:39 +0300

    Updates

---
+++ precache-manifest.b0583590313592ace9e0801aa07f2775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673d61271e3c7bbdf6b1a859a8e60c6b",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "07d5db212c64f35b06b9",
+    "url": "/task-manager/static/css/main.aaa4bbda.chunk.css"
+  },
+  {
+    "revision": "b13bc0eaf8298ffb04fc",
+    "url": "/task-manager/static/js/2.3f9b7958.chunk.js"
+  },
+  {
+    "revision": "07d5db212c64f35b06b9",
+    "url": "/task-manager/static/js/main.8e9375ce.chunk.js"
+  },
+  {
+    "revision": "0dabc0f1b9c3dcf7060f",
+    "url": "/task-manager/static/js/runtime-main.4277c2e9.js"
+  },
+  {
+    "revision": "39dab33fcea1bb9cd431730d3d820998",
+    "url": "/task-manager/static/media/edit.39dab33f.png"
+  }
+]);
